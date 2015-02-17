5f68656dcd16d73dd38adc5bf3f72fefe3616797
benwainwright <bwainwright28@gmail.com>
2015-02-17 18:13:32 +0000

    Bugfix
    Fixed bug where meta info was included with 'include_once' which means that it only worked on the first post...

--- post-layouts/post.php
+++ post-layouts/post.php
@@ -1,7 +1,7 @@
 <div itemscope itemytype="http://schema.org/Article" <?php post_class('post') ?>>
       <header>
           <h2><a href="<?php the_permalink() ?>"><span itemprop="name"><?php the_title(); ?></span></a></h2>
-          <?php include_once( __DIR__ . '/../snippets/post_meta.php' ) ?>
+          <?php include( __DIR__ . '/../snippets/post_meta.php' ) ?>
       </header>
       
       <?php if ( has_post_thumbnail() ) { the_post_thumbnail(); }
