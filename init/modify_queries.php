ae14d51738efaa9f51d5d6066f3e50e8b8068d2c
benwainwright <bwainwright28@gmail.com>
2015-02-23 11:05:45 +0000

    Refactor
    Move fixtures page into an archive template

--- init/modify_queries.php
+++ init/modify_queries.php
@@ -19,5 +19,26 @@ function modify_query( $query ) {
 		$query->set( 'meta_query', $meta_query);
 
     }
+	
+	// Setup fixtures archive
+    if ( !is_admin() && $query->is_post_type_archive( 'fixtures' ) && $query->is_main_query() ) 
+    {
+
+        $query->set( 'orderby', 'meta_value' );
+		$query->set( 'meta_key', 'fixture-date' );
+		$query->set('nopaging', 1);
+		$query->set( 'order', 'ASC' );
+		$taxonomy = get_terms ( array ( 'seasons' ) );
+		foreach ($taxonomy as $tax) $taxeslight[] = $tax->slug;
+		$meta_query = array(
+					    array(
+					        'taxonomy' => 'seasons',
+					        'field'    => 'slug',
+					        'terms'    => $taxeslight,
+					        'operator' => 'NOT IN'
+					    ));
+		$query->set('meta_query', $meta_query);
+	}
+
 }
 add_action( 'pre_get_posts', 'modify_query' );
