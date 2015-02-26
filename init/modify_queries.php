515e73bf271174f76afa6c6acbab6675c801a923
benwainwright <bwainwright28@gmail.com>
2015-02-26 19:26:56 +0000

    Bugfix
    Put season archive into an order

--- init/modify_queries.php
+++ init/modify_queries.php
@@ -1,6 +1,14 @@
 <?php
 function modify_query( $query ) {
 	
+	// Setup seasons archive page
+	if ( !is_admin() && is_tax( 'seasons') && $query->is_main_query() )
+	{
+        $query->set( 'orderby', 'meta_value' );
+		$query->set( 'meta_key', 'fixture-date' );
+		$query->set('nopaging', 1);
+	}
+	
 	// Setup events archive
     if ( !is_admin() && $query->is_post_type_archive( 'events' ) && $query->is_main_query() ) 
     {
