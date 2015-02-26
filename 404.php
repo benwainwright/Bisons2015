2c0b5d7bfd966dcf8ca0faa6f0e2db7e9fc12115
benwainwright <bwainwright28@gmail.com>
2015-02-26 19:38:45 +0000

    Layout Fix
    Changed the 404 page

--- 404.php
+++ 404.php
@@ -3,10 +3,10 @@
 <div id="wrapper">
 	<div id="pagecol">
         <div class='page'>
-   
-            <p class="infoalert">Sorry... but the content you were looking for isn't here. Redirecting you to our homepage, please wait...</p>
+   			<h2>Whoops!</h2>
+            <p class="infoalert">Whatever you tried to find isn't here anymore, unfortunately! If you wait a moment, you will be redirected to our home page...</p>
             <script type="text/javascript">
-                setTimeout( function() { window.location.replace ( '<?php echo $GLOBALS['blog_info']['url'] ?>' )}, 5000 );
+               setTimeout( function() { window.location.replace ( '<?php echo $GLOBALS['blog_info']['url'] ?>' )}, 5000 );
             </script>
         </div>
     </div>
