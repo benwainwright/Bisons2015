ded8e90076ab4e4a97df073c8c3b8b40664f5654
benwainwright <bwainwright28@gmail.com>
2015-06-07 02:42:35 +0100

    New Feature
    Added 'every day at 7pm' schedule

--- wp-cron/activateSchedules.php
+++ wp-cron/activateSchedules.php
@@ -14,12 +14,19 @@ function activateSchedules()
 
 		$hook = explode('-', pathinfo( $hook )['filename']);
 		$name = $hook[0];
+
 		$schedule = $hook[1];
+
+		$schedules = wp_get_schedules();
+
+		$time = isset($schedules[$schedule]['startTime']) ?
+			strtotime('tomorrow', strtotime('midnight')) + 1 + $schedules[$schedule]['startTime'] :
+			time();
 
 		$timestamp = wp_next_scheduled( $name );
 
 		if ( ! $timestamp || $timestamp < time() ) {
-			wp_schedule_event( time(), 'schedule' . $schedule, $name );
+			wp_schedule_event( $time, 'schedule' . $schedule, $name );
 		}
 	}
 
