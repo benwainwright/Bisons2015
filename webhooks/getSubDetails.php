beb1ab39397b6de84b276d76a8c92e326d029c28
benwainwright <bwainwright28@gmail.com>
2015-06-13 22:34:38 +0100

    debug

--- webhooks/getSubDetails.php
+++ webhooks/getSubDetails.php
@@ -1,64 +1,71 @@
 <?php
 
-$query = new WP_Query( array( 'post_type' => 'GCLBillLog', 'posts_per_page' => -1) );
+$query = new WP_Query( array( 'post_type' => 'GCLBillLog', 'posts_per_page' => - 1 ) );
 
-while ($query->have_posts()) {
+while ( $query->have_posts() ) {
 
 	$query->the_post();
 
-	$id = get_the_author_meta('ID');
+	$id = get_the_author_meta( 'ID' );
 
 
-	if ( get_post_meta( get_the_id(), 'source_id', true) && !get_user_meta( $id, 'GCLsubscriptionStatus', true) ) {
+	if ( get_post_meta( get_the_id(), 'source_id', true ) ) {
 
-		update_user_meta($id, 'payMethod', 'dd' );
+		if ( ! get_user_meta( $id, 'GCLsubscriptionStatus', true ) ){
 
-		if ('subscription' === get_post_meta( get_the_id(), 'source_type', true) ) {
+		update_user_meta( $id, 'payMethod', 'dd' );
+
+		
+
+		if ( 'subscription' === get_post_meta( get_the_id(), 'source_type', true ) ) {
 
 			try {
-				$source = GoCardless_Subscription::find( get_post_meta( get_the_id(), 'source_id', true) );
-			} catch( GoCardless_ApiException $e ) {
+				$source = GoCardless_Subscription::find( get_post_meta( get_the_id(), 'source_id', true ) );
+			} catch ( GoCardless_ApiException $e ) {
 
 
 				$error = array(
-					'code' => $e->getCode,
+					'code'  => $e->getCode,
 					'error' => $e->getMessage(),
 					'file'  => $e->getFile(),
 					'line'  => $e->getLine(),
-					'trace'  => $e->getTrace()
+					'trace' => $e->getTrace()
 				);
 
-				new dBug($error);
-			}		}
-
-		else {
-			if ('pre_authorization' === get_post_meta( get_the_id(), 'source_type', true) ) {
-				try{
-					$source = GoCardless_PreAuthorization::find( get_post_meta( get_the_id(), 'source_id', true));
-				} catch( GoCardless_ApiException $e ) {
+				new dBug( $error );
+			}
+		} else {
+			if ( 'pre_authorization' === get_post_meta( get_the_id(), 'source_type', true ) ) {
+				try {
+					$source = GoCardless_PreAuthorization::find( get_post_meta( get_the_id(), 'source_id', true ) );
+				} catch ( GoCardless_ApiException $e ) {
 
 
 					$error = array(
-						'code' => $e->getCode,
+						'code'  => $e->getCode,
 						'error' => $e->getMessage(),
 						'file'  => $e->getFile(),
 						'line'  => $e->getLine(),
-						'trace'  => $e->getTrace()
+						'trace' => $e->getTrace()
 					);
 
-					new dBug($error);
+					new dBug( $error );
 				}
 
 			}
 		}
 
-		new dBug($source);
+		new dBug( $source );
 
-		update_user_meta($id, 'GCLsubscriptionStatus', $source->status);
+		update_user_meta( $id, 'GCLsubscriptionStatus', $source->status );
+
 	}
 
-	else {
-		update_user_meta($user->ID, 'singlePaymentID', get_post_meta(get_the_id(), 'id', true));
-		update_user_meta($id, 'payMethod', 'single' );
+
+	} else {
+
+
+		update_user_meta( $user->ID, 'singlePaymentID', get_post_meta( get_the_id(), 'id', true ) );
+		update_user_meta( $id, 'payMethod', 'single' );
 	}
 }
