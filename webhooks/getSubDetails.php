e919eb23553acbf5141a762c7f4ce1dfacd96642
benwainwright <bwainwright28@gmail.com>
2015-06-13 22:10:32 +0100

    Admin method

---
+++ webhooks/getSubDetails.php
@@ -0,0 +1,33 @@
+<?php
+
+$query = new WP_Query( array( 'post_type' => 'GCLBillLog', 'posts_per_page' => -1) );
+
+while ($query->have_posts()) {
+
+	$query->the_post();
+
+	$id = get_the_author_meta('ID');
+
+
+	if ( get_post_meta( get_the_id(), 'source_id', true) ) {
+
+		update_user_meta($id, 'payMethod', 'dd' );
+
+		if ('subscription' === get_post_meta( get_the_id(), 'source_type', true) ) {
+			$source = GoCardless_Subscription::find( $bill->source_id);
+		}
+
+		else {
+			if ('pre_authorization' === get_post_meta( get_the_id(), 'source_type', true) ) {
+				$source = GoCardless_PreAuthorization::find( $bill->source_id);
+			}
+		}
+
+		update_user_meta($id, 'GCLsubscriptionStatus', $source->status);
+	}
+
+	else {
+		update_user_meta($user->ID, 'singlePaymentID', get_post_meta(get_the_id(), 'id', true));
+		update_user_meta($id, 'payMethod', 'single' );
+	}
+}
