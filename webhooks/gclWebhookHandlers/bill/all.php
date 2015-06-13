d4daac4b4560af92d90de3db042307794c92f622
benwainwright <bwainwright28@gmail.com>
2015-06-13 01:29:11 +0100

    New Feature
    Update web hooks

--- webhooks/gclWebhookHandlers/bill/all.php
+++ webhooks/gclWebhookHandlers/bill/all.php
@@ -1,25 +1,46 @@
 <?php
 
-$date = date( 'Y-m-d H:i:s', isset ( $bill['paid_at'] ) ? strtotime( $resource['paid_at'] . ' UTC' ) : time() );
 
-// Create webhook log
-$hook_log = array(
-	'post_status' => 'publish',
-	'post_date' => $date,
-	'post_type' => 'GCLBillLog'
-);
 
-$hook_log['post_author'] = $user->ID;
+// Check if bill already exists
+$query = new WP_Query( array( 'post_type' => 'GCLBillLog', 'posts_per_page' => 1 ) );
 
-// Log webhook
-$id = wp_insert_post( $hook_log );
+if ($query->have_posts()) {
+	$query->the_post();
 
-update_post_meta($id, 'id', $resource['source_id']);
-update_post_meta($id, 'source_id', $resource['source_id']);
-update_post_meta($id, 'action', $data['action']);
-update_post_meta($id, 'status', $resource['status']);
-update_post_meta($id, 'amount', $resource['amount']);
-update_post_meta($id, 'amount_minus_fees', $resource['amount_minus_fees']);
-update_post_meta($id, 'source_type', $resource['source_type']);
+	if ( isset ( $bill['paid_at'] ) ) {
+		update_post_meta( get_the_id(), 'paid_at', strtotime( $resource['paid_at'] . ' UTC'));
+	}
 
-$return = array('log_post_id' => $id);
+	update_post_meta( get_the_id(), 'action', $data['action'] );
+	update_post_meta( get_the_id(), 'status', $resource['status'] );
+
+}
+
+else {
+
+	// Create new webhook log
+	$hook_log = array(
+		'post_status' => 'publish',
+		'post_date'   => $date,
+		'post_type'   => 'GCLBillLog'
+	);
+
+	$hook_log['post_author'] = $user->ID;
+
+	$date = date( 'Y-m-d H:i:s');
+
+	// Log webhook
+	$id = wp_insert_post( $hook_log );
+
+	update_post_meta( $id, 'id', $resource['source_id'] );
+	update_post_meta( $id, 'source_id', $resource['source_id'] );
+	update_post_meta( $id, 'action', $data['action'] );
+	update_post_meta( $id, 'status', $resource['status'] );
+	update_post_meta( $id, 'amount', $resource['amount'] );
+	update_post_meta( $id, 'amount_minus_fees', $resource['amount_minus_fees'] );
+	update_post_meta( $id, 'source_type', $resource['source_type'] );
+
+	$return = array( 'log_post_id' => $id );
+
+}
