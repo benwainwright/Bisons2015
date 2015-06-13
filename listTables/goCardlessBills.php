81fb35f9215a07b46589d39f9e7f94a1d0289c08
benwainwright <bwainwright28@gmail.com>
2015-06-13 09:25:16 +0100

    Bugfix

--- listTables/goCardlessBills.php
+++ listTables/goCardlessBills.php
@@ -125,7 +125,7 @@ class GCLBillsTable extends WP_List_Table_Copy
 		{
 			case 'amount': case 'amount_minus_fees':
 				setlocale(LC_MONETARY, 'en_GB.UTF-8');
-				return money_format( '%n', $item [ $column_name ]);
+				return money_format( '%n', (int) $item [ $column_name ]);
 			break;
 
 			case 'user':
