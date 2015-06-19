f55664ab254a70338b3292099ba83e90d5de3b24
benwainwright <bwainwright28@gmail.com>
2015-06-19 19:09:52 +0100

    New feature
    Added ability to modify payment info from subs page

--- player-pages/your-subs.php
+++ player-pages/your-subs.php
@@ -8,6 +8,8 @@
 </header>
 
 <p>Please find below details of all payments made via the online membership system. If you have any questions about any of this information, please do not hesitate to contact a member of the committee.</p>
+
+<form method="POST">
 <table class='verticalTable'>
 	<tbody>
 		<tr>
@@ -26,12 +28,46 @@
 		<?php endif ?>
 		<tr>
 			<th>Type</th>
-			<td><?php echo $d['payWhen'] ?></td>
+			<td>
+				<select name='payWhen'>
+					<?php selectOptionFromMeta($d['user'], 'payWhen' , 'first', 'First day of Month') ?>
+					<?php selectOptionFromMeta($d['user'], 'payWhen' , 'last', 'Last day of Month') ?>
+					<?php selectOptionFromMeta($d['user'], 'payWhen' , 'specificDay', 'Specific day of Month') ?>
+					<?php selectOptionFromMeta($d['user'], 'payWhen' , 'specificWeekDay', 'Specific weekday of Month') ?>
+				</select>
+			</td>
 		</tr>
+
 		<tr>
 			<th>Day of Month</th>
-			<td><?php echo $d['dayOfMonth'] ?></td>
+			<td>
+				<select name='dayOfMonth'>
+				<?php for ($i = 1; $i <= 31; $i++) : ?>
+					<?php selectOptionFromMeta($d['user'], 'dayOfMonth' , $i) ?>
+				<?php endfor ?>
+				</select>
+			</td>
 		</tr>
+
+		<tr>
+			<th>Which Weekday</th>
+			<td>
+				<select name='whichWeekDay'>
+					<?php selectOptionFromMeta($d['user'], 'whichWeekDay' , 'first', '1st') ?>
+					<?php selectOptionFromMeta($d['user'], 'whichWeekDay' , 'second', '2nd') ?>
+					<?php selectOptionFromMeta($d['user'], 'whichWeekDay' , 'third', '3rd') ?>
+					<?php selectOptionFromMeta($d['user'], 'whichWeekDay' , 'fourth', '4th') ?>
+					<?php selectOptionFromMeta($d['user'], 'whichWeekDay' , 'fifth', '5th') ?>
+				</select>
+				<select name='weekDay'>
+					<?php for ($i = 0; $i <= 6; $i++) : ?>
+						<?php selectOptionFromMeta($d['user'], 'weekDay' , jddayofweek($i, 1)) ?>
+					<?php endfor ?>
+				</select>
+			</td>
+		</tr>
+
+
 
 		<tr>
 			<th>Next Payment Date</th>
@@ -39,6 +75,8 @@
 		</tr>
 	</tbody>
 </table>
+<button type="submit">Update</button>
+</form>
 
 <p>Although you have filled in a membership form, it appears that you don't have an active payment subscription. Use the form below to set one up.</p>
 
