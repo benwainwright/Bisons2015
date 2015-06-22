0ab5cbf2a3662cbf1d91d0b90906e6ada58df4cf
benwainwright <bwainwright28@gmail.com>
2015-06-22 07:33:41 +0100

    Minor Tweak
    Allow admin to modify user monthly payment via profile

--- init/user-contactmethods.php
+++ init/user-contactmethods.php
@@ -3,6 +3,8 @@ function modifyContactMethods( $fields ) {
 
 	if (current_user_can('modify_gcl_user_id')) {
 		$fields['GCLUserID'] = 'GoCardless User ID';
+		$fields['currentFee'] = 'Monthly DD Payment';
+
 	}
 
 	return $fields;
