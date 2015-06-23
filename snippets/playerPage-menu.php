0ade5462b9315b2dcfcf5a91ffb668baaa94f9e1
benwainwright <bwainwright28@gmail.com>
2015-06-23 20:25:34 +0100

    Tweak
    Tweaking FA Icons

--- snippets/playerPage-menu.php
+++ snippets/playerPage-menu.php
@@ -1,20 +1,21 @@
 <ul class='pageMenu'>
 
 	<?php global $pagename; if ( $pagename != '' ) :  ?>
-		<li><a class="fa    fa-arrow-circle-left fa-lg" href="<?php echo site_url('players-area') ?>">Player's Area</a></li>
+		<li><a href="<?php echo site_url('players-area') ?>"><i class="fa fa-arrow-circle-left"></i>Player's Area</a></li>
 	<?php endif ?>
 	<?php if ( current_user_can('edit_post', get_the_id()) ) { ?>
-		<li><a class='fa fa-plus-square fa-lg' href='<?php echo $GLOBALS['blog_info']['url']; ?>/wp-admin/post-new.php?post_type=player-page'>Add</a></li>
-		<li><a class='fa fa-wordpress fa-lg' href='<?php echo admin_url(); ?>'>Dashboard</a></li>
+		<li><a href='<?php echo $GLOBALS['blog_info']['url']; ?>/wp-admin/post-new.php?post_type=player-page'><i class='fa fa-plus-square' ></i>Add</a></li>
+		<li><a href='<?php echo admin_url(); ?>'><i class='fa fa-wordpress'></i>Dashboard</a></li>
 
 
 	<?php } ?>
 	<?php if (get_user_meta(get_current_user_id(), 'joined', true)) : ?>
-		<li><a class='fa fa-credit-card fa-lg' href='<?php echo site_url('players-area/your-subs') ?>'>Subs</a></li>
-		<li><a class='fa fa-info fa-lg' href='<?php echo site_url('players-area/membership-form') ?>'>Your Details</a></li>
+		<li><a href='<?php echo site_url('players-area/your-subs') ?>'><i class='fa fa-credit-card'></i>Subs</a></li>
+		<li><a href='<?php echo site_url('players-area/membership-form') ?>'><i class='fa fa-info'></i>Details</a></li>
 	<?php else : ?>
-		<li><a class='fa fa-info fa-lg' href='<?php echo site_url('players-area/membership-form') ?>'>Join</a></li>
+		<li><a href='<?php echo site_url('players-area/membership-form') ?>'><i class='fa fa-info'></i>Join</a></li>
 	<?php endif ?>
-	<li><a class='fa fa-user fa-lg' href='<?php echo site_url('players-area/player-profile') ?>'>Profile</a></li>
+	<li><a href='<?php echo site_url('players-area/player-profile') ?>'><i class='fa fa-user'></i>Profile</a></li>
+	<li><a href='<?php echo site_url('players-area/change-password') ?>'><i class='fa fa-key'></i>Password</a></li>
 
 </ul>
