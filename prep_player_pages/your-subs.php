7238c963a00971123c7c7f11c7d2b8d39a4b925b
benwainwright <bwainwright28@gmail.com>
2015-06-25 15:48:29 +0100

    New feature
    Insert fees into subs form

--- prep_player_pages/your-subs.php
+++ prep_player_pages/your-subs.php
@@ -17,7 +17,55 @@ if ( get_user_meta( $formUser, 'joined', true ) ) {
 
 	$data = &$wp_query->query['bisons_data'];
 
-	$data['user']            = $form_user;
+
+	$fees = new WP_Query ( array( 'post_type' => 'membership_fee', 'nopaging' => true ) );
+	while ( $fees->have_posts() ) {
+		$fees->the_post();
+
+		$the_fee = array(
+			'id'              => get_the_id(),
+			'name'            => get_post_meta( get_the_id(), 'fee-name', true ),
+			'initial-payment' => get_post_meta( get_the_id(), 'initial-payment', true ),
+			'amount'          => get_post_meta( get_the_id(), 'fee-amount', true ),
+			'description'     => get_post_meta( get_the_id(), 'fee-description', true )
+		);
+
+		$isSupporter = get_post_meta( get_the_id(), 'supporter-player', true ) === 'Supporter';
+		$isPlayer    = get_post_meta( get_the_id(), 'supporter-player', true ) === 'Player';
+		$isMonthly   = get_post_meta( get_the_id(), 'fee-type', true ) == "Monthly Direct Debit";
+
+		if ( $isSupporter && $isMonthly ) {
+			$data['supporterFees']['direct_debits'] [] = $the_fee;
+		} else if ( $isSupporter && ! $isMonthly ) {
+			$data['supporterFees']['single_payments'] [] = $the_fee;
+		} else if ( $isPlayer && $isMonthly ) {
+			$data['playerFees']['direct_debits'] [] = $the_fee;
+		} else if ( $isPlayer && ! $isMonthly ) {
+			$data['playerFees']['single_payments'] [] = $the_fee;
+		}
+
+	}
+
+	$method = get_user_meta( $formUser, 'payMethod', true );
+
+	if ( 'dd' === $method ) {
+
+		$feeid = ( get_user_meta( $formUser, 'playermembershiptypemonthly', true ) != '' )
+			? get_user_meta( $formUser, 'playermembershiptypemonthly', true )
+			: get_user_meta( $formUser, 'supportermembershiptypemonthly', true );
+
+	} elseif ( 'sp' == $method ) {
+
+		$feeid = ( get_user_meta( $formUser, 'playermembershiptypesingle', true ) != '' )
+			? get_user_meta( $formUser, 'playermembershiptypesingle', true )
+			: get_user_meta( $formUser, 'supportermembershiptypesingle', true );
+
+	}
+
+	$data['description'] = get_post_meta($feeid, 'fee-description', true);
+
+
+	$data['user']            = $formUser;
 	$data['joined']          = true;
 	$data['payMethod']       = get_user_meta( $form_user, 'payMethod', true ) ? get_user_meta( $form_user,
 		'payMethod', true ) : false;
