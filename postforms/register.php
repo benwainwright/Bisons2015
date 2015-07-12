676a82d8f62d81bf317ac8b3394a7c731224bb72
benwainwright <bwainwright28@gmail.com>
2015-07-12 14:57:57 +0100

    Removed formatting changes

--- postforms/register.php
+++ postforms/register.php
@@ -1,76 +1,66 @@
 <?php
-wp_enqueue_script( 'formvalidation' );
-wp_enqueue_script( 'chosen_init' );
-wp_enqueue_style( 'chosen_css' );
+wp_enqueue_script('formvalidation');
+wp_enqueue_script('chosen_init');
+wp_enqueue_style('chosen_css');
 ?>
 
 <div id='custom-form'>
-	<p>Please <a href='<?php echo admin_url( 'admin.php?page=add-player' ) ?>'>add any new players</a> to the database
-		before recording the register to ensure their attendance is recorded also.</p>
-	<table class="form-table">
-		<tbody>
-		<tr class='smallFormRow'>
-			<th><label for="register_date">Date</label></th>
+	<p>Please <a href='<?php echo admin_url('admin.php?page=add-player') ?>'>add any new players</a> to the database before recording the register to ensure their attendance is recorded also.</p>
+    <table class="form-table">
+        <tbody>
+            <tr class='smallFormRow'>
+            	<th><label for="register_date">Date</label></th>
+                <td>
+                    <input type='date' class="required" name='reg-date' value='<?php echo get_post_meta( $post->ID, 'reg-date', true) ? date('Y-m-d', get_post_meta( $post->ID, 'reg-date', true) ) : date('Y-m-d');     ?>' />
+	                <span class="description">The date of the training session.</span>
+
+                </td>
+            </tr>
+
+			<tr>
+            <th><label for="players_present">Present</label></th>
 			<td>
-				<input type='date' class="required" name='reg-date'
-				       value='<?php echo get_post_meta( $post->ID, 'reg-date', true ) ? date( 'Y-m-d',
-					       get_post_meta( $post->ID, 'reg-date', true ) ) : date( 'Y-m-d' ); ?>'/>
-				<span class="description">The date of the training session.</span>
+			<?php $selected = get_post_meta ( $post->ID, 'players_present', false ) ?>
 
-			</td>
-		</tr>
-
-		<tr>
-			<th><label for="players_present">Present</label></th>
-			<td>
-				<?php $selected = get_post_meta( $post->ID, 'players_present', false ) ?>
-
-				<select class='register_listbox' id='players_present' multiple="multiple" name="players_present[]">
-					<?php $users = get_users();
-					foreach ( $users as $user ) : ?>
-						<option <?php if ( array_search( $user->data->ID, $selected ) !== false ) {
-							echo "selected='selected' "} ?>value='<?php echo $user->data->ID ?>'><?php echo $user->data->display_name ?></option>
-					<?php endforeach; ?>
-				</select>
+			    <select class='register_listbox' id='players_present' multiple="multiple" name="players_present[]">
+					<?php $users = get_users(); foreach ($users as $user) : ?>
+					<option <?php if ( array_search ($user->data->ID, $selected) !== false ) echo "selected='selected' " ?>value='<?php echo $user->data->ID?>'><?php echo $user->data->display_name ?></option>
+			        <?php endforeach; ?>
+			    </select>
 				<span class="description">Players that attended and took part in the session.</span>
 			</td>
-		</tr>
-		<tr>
-			<th><label for="players_watching">Watching</label></th>
+			</tr>
+			<tr>
+            <th><label for="players_watching">Watching</label></th>
 			<td>
 
-				<?php $selected = get_post_meta( $post->ID, 'players_watching', false ) ?>
+			<?php $selected = get_post_meta ( $post->ID, 'players_watching', false ) ?>
 
-				<select class='register_listbox' id='players_watching' multiple="multiple" name="players_watching[]">
-					<?php $users = get_users();
-					foreach ( $users as $user ) : ?>
-						<option <?php if ( array_search( $user->data->ID, $selected ) !== false ) {
-							echo "selected='selected' "} ?>value='<?php echo $user->data->ID ?>'><?php echo $user->data->display_name ?></option>
-					<?php endforeach; ?>
-				</select>
+			    <select class='register_listbox' id='players_watching' multiple="multiple" name="players_watching[]">
+					<?php $users = get_users(); foreach ($users as $user) : ?>
+					<option <?php if ( array_search ($user->data->ID, $selected) !== false ) echo "selected='selected' " ?>value='<?php echo $user->data->ID?>'><?php echo $user->data->display_name ?></option>
+			        <?php endforeach; ?>
+			    </select>
 				<span class="description">Players that attended and didn't take part in the session.</span>
 			</td>
-		</tr>
-		<tr>
-			<th><label for="players_coaching">Coaching</label></th>
+			</tr>
+			<tr>
+            <th><label for="players_coaching">Coaching</label></th>
 			<td>
 
-				<?php $selected = get_post_meta( $post->ID, 'players_coaching', false ) ?>
+			<?php $selected = get_post_meta ( $post->ID, 'players_coaching', false ) ?>
 
-				<select class='register_listbox' id='players_coaching' multiple="multiple" name="players_coaching[]">
-					<?php $users = get_users();
-					foreach ( $users as $user ) : ?>
-						<option <?php if ( array_search( $user->data->ID, $selected ) !== false ) {
-							echo "selected='selected' "} ?>value='<?php echo $user->data->ID ?>'><?php echo $user->data->display_name ?></option>
-					<?php endforeach; ?>
-				</select>
+			    <select class='register_listbox' id='players_coaching' multiple="multiple" name="players_coaching[]">
+					<?php $users = get_users(); foreach ($users as $user) : ?>
+					<option <?php if ( array_search ($user->data->ID, $selected) !== false ) echo "selected='selected' " ?>value='<?php echo $user->data->ID?>'><?php echo $user->data->display_name ?></option>
+			        <?php endforeach; ?>
+			    </select>
 				<span class="description">Players that were involved in coaching the session.</span>
 			</td>
-		</tr>
-		<?php if ( $absent = get_post_meta( $post->ID, 'players_absent', false ) ) : ?>
-			<tr>
-				<th><label for="players_absent">Absent</label></th>
-				<td><span class="description">
+			</tr>
+			<?php if ( $absent = get_post_meta ( $post->ID, 'players_absent', false ) ) :  ?>
+	            <th><label for="players_absent">Absent</label></th>
+	            <td><span class="description">
 		            <?php
 		            $users = get_users();
 		            foreach ( $users as $user ) {
@@ -79,20 +69,17 @@ wp_enqueue_style( 'chosen_css' );
 			            }
 		            }
 
-		            echo implode( ', ', $absentPlayerNames );
+		            echo implode(', ', $absentPlayerNames);
 		            ?></span>
-				</td>
+	            </td>
+            </tr>
+            <?php endif ?>
+
+            <?php if( !current_user_can( 'advanced_posting_layout' ) ) : ?>
+        	<tr>
+			<td class='formButtonCell' colspan='2'><input type="submit" name="publish" id="publish" class="button button-primary button-large resultsButton" value="Save" accesskey="p"></div></td>
 			</tr>
-		<?php endif ?>
+			<?php endif ?>
 
-		<?php if ( ! current_user_can( 'advanced_posting_layout' )) : ?>
-		<tr>
-			<td class='formButtonCell' colspan='2'><input type="submit" name="publish" id="publish"
-			                                              class="button button-primary button-large resultsButton"
-			                                              value="Save" accesskey="p">
-</div></td>
-
-<?php endif ?>
-
-</table>
-</div>
+        </table>
+    </div>
