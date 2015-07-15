11be743a33c7ebb9dedb346c4cbefd2f4f493a9b
benwainwright <bwainwright28@gmail.com>
2015-07-15 10:15:34 +0100

    New features
    Formatting changes. Also now allows editing of user meta

--- player-pages/player-profile.php
+++ player-pages/player-profile.php
@@ -2,61 +2,67 @@
 wp_enqueue_script( 'dynamicforms' );
 wp_enqueue_script( 'formvalidation' );
 
+$ppForm = new Wordpress_Form ( 'playerprofile', 'name', 'post', 'Submit', 'submit_player_profile' );
 
-$ppform = new Wordpress_Form ( 'playerprofile', 'name', 'post', 'Submit', 'submit_player_profile' );
 
-$ppform->add_fieldset( 'aboutyou', 'About You' );
-$ppform->add_file_upload( 'aboutyou', 'photo', 'Photograph', 'image_id' );
-$ppform->add_text_input( 'aboutyou', 'name', 'First Name *', 'notempty' );
-$ppform->add_text_input( 'aboutyou', 'nickname', 'Club Nickname' );
-$ppform->add_text_input( 'aboutyou', 'age', 'Age *', 'notempty' );
-$ppform->add_text_input( 'aboutyou', 'living', 'What do you do for a living?' );
+$ppForm->isEditorFor('currentUserMeta');
 
-$ppform->add_fieldset( 'youandthebisons', 'You and the Bisons' );
-$ppform->add_text_input( 'youandthebisons', 'jexp',
+
+$ppForm->add_fieldset( 'aboutyou', 'About You' );
+$ppForm->add_file_upload( 'aboutyou', 'photo', 'Photograph', 'image_id' );
+$ppForm->add_text_input( 'aboutyou', 'ppName', 'First Name *', 'notempty' );
+$ppForm->add_text_input( 'aboutyou', 'nickname', 'Club Nickname' );
+$ppForm->add_text_input( 'aboutyou', 'age', 'Age *', 'notempty' );
+$ppForm->add_text_input( 'aboutyou', 'living', 'What do you do for a living?' );
+
+$ppForm->add_fieldset( 'youandthebisons', 'You and the Bisons' );
+$ppForm->add_text_input( 'youandthebisons', 'jexp',
 	'How much did you know about the game when you first started playing with the Bisons? *', 'notempty' );
-$ppform->add_text_input( 'youandthebisons', 'whydoyouplay', 'Why do you play for The Bisons?' );
-$ppform->add_text_input( 'youandthebisons', 'bestmem', 'Best Bisons memory or acheivement?' );
-$ppform->add_text_input( 'youandthebisons', 'position',
+$ppForm->add_text_input( 'youandthebisons', 'whydoyouplay', 'Why do you play for The Bisons?' );
+$ppForm->add_text_input( 'youandthebisons', 'bestmem', 'Best Bisons memory or acheivement?' );
+$ppForm->add_text_input( 'youandthebisons', 'position',
 	'What is your usual position? (Interpret that however you want...)' );
 
-$ppform->add_fieldset( 'youandrugby', 'You and the Bisons',
+$ppForm->add_fieldset( 'youandrugby', 'You and the Bisons',
 	'We appreciate that some of you may be very new to the game. If you can\'t answer some of these, just leave them blank, with the exception of those fields with an asterisk (*) next to the label.' );
-$ppform->add_text_input( 'youandthebisons', 'exp', 'How long have you been playing rugby? *', 'notempty' );
-$ppform->add_text_input( 'youandthebisons', 'followed', 'Which club or team do you follow?' );
-$ppform->add_text_input( 'youandthebisons', 'proplayerasp',
+$ppForm->add_text_input( 'youandthebisons', 'exp', 'How long have you been playing rugby? *', 'notempty' );
+$ppForm->add_text_input( 'youandthebisons', 'followed', 'Which club or team do you follow?' );
+$ppForm->add_text_input( 'youandthebisons', 'proplayerasp',
 	'Which professional player would you like to perform like, and why?' );
-$ppform->add_text_input( 'youandthebisons', 'proplayer',
+$ppForm->add_text_input( 'youandthebisons', 'proplayer',
 	'Which professional player <strong>do</strong> you perform like and why?' );
-$ppform->add_text_input( 'youandthebisons', 'superst', 'Do you have any prematch superstitions/routines?' );
+$ppForm->add_text_input( 'youandthebisons', 'superst', 'Do you have any prematch superstitions/routines?' );
 
-$ppform->add_fieldset( 'theinterview', 'The Interview' );
-$ppform->add_text_input( 'theinterview', 'chatup', 'What is your best chat up line?' );
-$ppform->add_text_input( 'theinterview', 'breakfast', 'What do you normally eat for breakfast?' );
-$ppform->add_text_input( 'theinterview', 'possessions', 'What is your most treasured possession?' );
-$ppform->add_text_input( 'theinterview', 'notholiday', 'Where is the one place you would never go on holiday?' );
+$ppForm->add_fieldset( 'theinterview', 'The Interview' );
+$ppForm->add_text_input( 'theinterview', 'chatup', 'What is your best chat up line?' );
+$ppForm->add_text_input( 'theinterview', 'breakfast', 'What do you normally eat for breakfast?' );
+$ppForm->add_text_input( 'theinterview', 'possessions', 'What is your most treasured possession?' );
+$ppForm->add_text_input( 'theinterview', 'notholiday', 'Where is the one place you would never go on holiday?' );
 
-$ppform->add_text_input( 'theinterview', 'movielife', 'In the movie of your life, who would you be played by?' );
-$ppform->add_text_input( 'theinterview', 'growingup', 'When you were growing up, what did you want to be?' );
-$ppform->add_text_input( 'theinterview', 'filmcry', 'Name the last film that made you cry?' );
-$ppform->add_text_input( 'theinterview', 'eventfromhistory',
+$ppForm->add_text_input( 'theinterview', 'movielife', 'In the movie of your life, who would you be played by?' );
+$ppForm->add_text_input( 'theinterview', 'growingup', 'When you were growing up, what did you want to be?' );
+$ppForm->add_text_input( 'theinterview', 'filmcry', 'Name the last film that made you cry?' );
+$ppForm->add_text_input( 'theinterview', 'eventfromhistory',
 	'If you could turn back time and witness one event from history, what would it be?' );
-$ppform->add_text_input( 'theinterview', 'sigdish', 'What is your signature dish?' );
-$ppform->add_text_input( 'theinterview', 'lastmeal', 'What would your last meal be?' );
-$ppform->add_text_input( 'theinterview', 'desertisland',
+$ppForm->add_text_input( 'theinterview', 'sigdish', 'What is your signature dish?' );
+$ppForm->add_text_input( 'theinterview', 'lastmeal', 'What would your last meal be?' );
+$ppForm->add_text_input( 'theinterview', 'desertisland',
 	'Stranded on a desert island, what would your three essential items be?' );
-$ppform->add_text_input( 'theinterview', 'lastfifty', 'What would you buy with your last fifty pounds?' );
+$ppForm->add_text_input( 'theinterview', 'lastfifty', 'What would you buy with your last fifty pounds?' );
 
-$ppform->add_fieldset( 'theteam', 'The Team', 'Out of the current team, who...' );
-$ppform->add_text_input( 'theteam', 'bestplayer', 'Is the best player?' );
-$ppform->add_text_input( 'theteam', 'fastestplayer', 'Is the fastest?' );
-$ppform->add_text_input( 'theteam', 'longestshower', 'Takes the longest to shower?' );
-$ppform->add_text_input( 'theteam', 'biggestmoaner', 'Is the biggest moaner?' );
-$ppform->add_text_input( 'theteam', 'dresssense', 'Has the worst dress sense?' );
-$ppform->add_text_input( 'theteam', 'lasttobar', 'Is the last to the bar?' );
-$ppform->add_text_input( 'theteam', 'worstdancer', 'Is the worst dancer?' );
-$ppform->add_text_input( 'theteam', 'badinfluence', 'Is the worst influence?' );
-$ppform->add_text_input( 'theteam', 'cheesegrindr', 'Has the cheesiest Grindr profile?' );
+$ppForm->add_fieldset( 'theteam', 'The Team', 'Out of the current team, who...' );
+$ppForm->add_text_input( 'theteam', 'bestplayer', 'Is the best player?' );
+$ppForm->add_text_input( 'theteam', 'fastestplayer', 'Is the fastest?' );
+$ppForm->add_text_input( 'theteam', 'longestshower', 'Takes the longest to shower?' );
+$ppForm->add_text_input( 'theteam', 'biggestmoaner', 'Is the biggest moaner?' );
+$ppForm->add_text_input( 'theteam', 'dresssense', 'Has the worst dress sense?' );
+$ppForm->add_text_input( 'theteam', 'lasttobar', 'Is the last to the bar?' );
+$ppForm->add_text_input( 'theteam', 'worstdancer', 'Is the worst dancer?' );
+$ppForm->add_text_input( 'theteam', 'badinfluence', 'Is the worst influence?' );
+$ppForm->add_text_input( 'theteam', 'cheesegrindr', 'Has the cheesiest Grindr profile?' );
+
+
+
 
 
 ?>
@@ -66,18 +72,15 @@ $ppform->add_text_input( 'theteam', 'cheesegrindr', 'Has the cheesiest Grindr pr
 
 </header>
 <?php get_template_part( 'snippets/playerPage', 'flashMessages' ) ?>
-<?php if ( $ppform->is_errors() ) : ?>
+<?php if ( $ppForm->is_errors() ) : ?>
 	<ul class='formerrors'>
-		<?php foreach ( $ppform->get_form_errors() as $error ) : ?>
+		<?php foreach ( $ppForm->get_form_errors() as $error ) : ?>
 			<li><?php echo $error ?></li><?php endforeach ?>
 	</ul>
 <?php endif ?>
 
-<?php if ( $ppform->submit_form() ) :
-
-	?>
-	<p class='flashmessage'>Your profile has been submitted. It may not be published straight away, as it will need to
-		be reviewed by a member of the committe first.</p>
+<?php if ( $ppForm->submit_form() ) : ?>
+	<p class='flashmessage'>Profile saved.</p>
 <?php endif ?>
 
 <p class="important">To be featured on the 'player profiles' section of the website, have a go at filling out the
@@ -88,5 +91,5 @@ $ppform->add_text_input( 'theteam', 'cheesegrindr', 'Has the cheesiest Grindr pr
 	to say.</p>
 
 <?php
-$ppform->form_output();
+$ppForm->form_output();
 ?>
