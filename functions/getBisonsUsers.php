1241366e5e88aa243c0315d856669882dd343342
benwainwright <bwainwright28@gmail.com>
2015-07-17 10:26:45 +0100

    New feature
    getBisonsUsers can now get inactive users as well

--- functions/getBisonsUsers.php
+++ functions/getBisonsUsers.php
@@ -1,6 +1,13 @@
 <?php
 
-function getBisonsUsers()
+function getBisonsUsers( $inactive = false )
 {
-	return get_users( array('meta_key' => 'inActive', 'meta_compare' => 'NOT EXISTS'));
+
+	if ( ! $inactive ) {
+		return get_users( array( 'meta_key' => 'inActive', 'meta_compare' => 'NOT EXISTS' ) );
+	}
+
+	else {
+		return get_users( array( 'meta_key' => 'inActive', 'meta_compare' => 'EXISTS' ) );
+	}
 }
