48e9311c202846cfaf2556968d04bad73ee2422e
benwainwright <bwainwright28@gmail.com>
2015-08-30 13:11:34 +0100

    Removed unused flash messages

--- init/displayFlashMessages.php
+++ init/displayFlashMessages.php
@@ -6,31 +6,16 @@ global $bisonsMembership;
 $status = $bisonsMembership->getStatus( get_current_user_id() );
 
 $hasJoined                      = get_user_meta( get_current_user_id(), 'joined', true );
-$noDD                           = 'None' === $status;
-$DDCancelled                    = 'cancelled' === $status;
 $validFlashMessageInQueryString = wp_verify_nonce( $_GET['nonce'],
 		'bisonsFlashmessageNonce' ) && isset( $_GET['flash'] );
 
 
 if ( $validFlashMessageInQueryString ) {
+	if ( ! $hasJoined ) {
+		$bisonPlayersFlashMessage[] = array(
+			'priority' => 1,
+			'message'  => "Looks like you are not yet a club member. Click the <strong>join</strong> link above to sign up!"
+		);
 
-	$bisonPlayersFlashMessage[] = array(
-		'priority' => 10000,
-		'message'  => $_GET['flash']
-	);
-
-} else if ( $hasJoined && ( $noDD || $DDCancelled ) ) {
-
-	$bisonPlayersFlashMessage[] = array(
-		'priority' => 1,
-		'message'  => "Although you have submitted a membership form you still need to organise payment or your payment has been cancelled. Click the 'subs' link above to get it sorted!"
-	);
-
-} else if ( ! $hasJoined ) {
-
-	$bisonPlayersFlashMessage[] = array(
-		'priority' => 1,
-		'message'  => "Looks like you are not yet a club member. Click the <strong>join</strong> link above to sign up!"
-	);
-
+	}
 }
