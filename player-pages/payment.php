493b78423494a07c9a02f370adf0a238a20213db
benwainwright <bwainwright28@gmail.com>
2015-08-30 12:54:21 +0100

    Layout tweaks to account for decoupling

--- player-pages/payment.php
+++
@@ -1,4 +0,0 @@
-<header>
-<h2>Setup Direct Debit</h2>
-<p>Bisons subscription fees are managed by an external service called <a title="GoCardless" href="https://www.gocardless.com/">GoCardless</a>. To set up a Direct Debit payment (or to pay your fee outright for the season) click on the buttons below.</p>
-<h3>Full Membership</h3>
